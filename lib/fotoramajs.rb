c245430b7623bd1ab66c9ff391183cf20b453a34
Andrey A.I. Sitnik <andrey@sitnik.ru>
2013-07-17 02:33:50 +0400

    Add fotorama images to precompile

--- lib/fotoramajs.rb
+++ lib/fotoramajs.rb
@@ -1,5 +1,11 @@
 # Used only for Ruby on Rails gem to tell, that gem contain `lib/assets`.
 module Fotoramajs
+  class Railtie < Rails::Railtie
+    initializer 'fotorama.config' do |app|
+      app.config.assets.precompile += ['fotorama.png', 'fotorama@2x.png']
+    end
+  end
+
   module Rails
     class Engine < ::Rails::Engine
     end
